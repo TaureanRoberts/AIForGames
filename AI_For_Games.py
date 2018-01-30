af28dc281d94e27e6f4edc214a57c6766d91bd1e
Taurean Roberts <taureanjroberts@gmail.com>
2018-01-30 17:55:45 -0600

    Finished the overloading functuons

--- AI_For_Games.py
+++ AI_For_Games.py
@@ -12,7 +12,16 @@ class Vector2(object):
     def __mul__(self, other):
         new_vect2 = Vector2(self.x_pos * other, self.y_pos *other)
         return new_vect2
-    def magn(self, other):
-        new_vect2 = Vector2(self.x_pos + other.x)
-    def norm(self, other):
+    def magnitude(self):
+        '''Gets the magnitude of the vector'''
+        magn = math.sqrt(self.x_pos * self.x_pos + self.y_pos * self.y_pos)
+        return magn
+    def normalize(self):
+        '''Normalizes the magnitude of the vector'''
+        mag = self.magnitude
+        norm = Vector2(self.x_pos / mag, self.y_pos / mag)
+        return norm
     def dot(self, other):
+        '''Dot operator overloader'''
+        spot = Vector2(self.x_pos * other.x_pos, self.y_pos * other.y_pos)
+        return spot
