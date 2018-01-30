cf50cae6cf74d54b4337802850c032ac012a68a7
Taurean Roberts <taureanjroberts@gmail.com>
2018-01-30 17:29:32 -0600

    5:30 Changes to overloaders

--- AI_For_Games.py
+++ AI_For_Games.py
@@ -1,16 +1,18 @@
+import math
 class Vector2(object):
     def __init__(self, lhs, rhs):
-        xpos = 
-        ypos = 
-    def add(self, lhs, rhs):
-        xpos = lhs
-        ypos = rhs
-    def sub(self, lhs, rhs):
-        return lhs - rhs
-    def scale(self, lhs, rhs):
-        return lhs * 2 and rhs * 2
-    def magn(self, lhs, rhs):
-        
-    def norm(self, lhs, rhs):
-        
-    def dot(self, lhs, rhs):
+        self.x_pos = lhs
+        self.y_pos = rhs
+    def __add__(self, other):
+        new_vect2 = Vector2(self.x_pos + other.x_pos, self.y_pos + other.y_pos)
+        return new_vect2
+    def __sub__(self, other):
+        new_vect2 = Vector2(self.x_pos - other.x_pos, self.y_pos - self.y_pos)
+        return new_vect2
+    def __mul__(self, other):
+        new_vect2 = Vector2(self.x_pos * other, self.y_pos *other)
+        return new_vect2
+    def magn(self, other):
+        new_vect2 = Vector2(self.x_pos + other.x)
+    def norm(self, other):
+    def dot(self, other):
