6769435c2cd8c575dbaad6764e19700a77ba7b2e
Taurean Roberts <taureanjroberts@gmail.com>
2018-02-14 16:19:40 -0600

    Finding G Score

--- a_node.py
+++ a_node.py
@@ -6,12 +6,18 @@ class Node(object):
         self.g_score = 0
         self.h_score = 0
         self.f_score = 0
-        self.is_traversable = traverseable
+        self.is_traversable = True
 
     def calculate_g_score(self, nodes):
-        self.position = nodes
+        if self.position.x_pos = nodes.position.x_pos
+            self.g_score = nodes.g_score + 10
+        elif self.position.y_pos = nodes.position.y_pos
+            self.g_score = nodes.g_score + 10
+        else
+            self.g_score = nodes.g_score + 14
 
     def node_parent(self, parents):
+        
         self.parent = parents
 
 class Graph(object):
@@ -23,6 +29,3 @@ class Graph(object):
         for i in range(0, self.dimension.x_pos):
             for j in range(0, self.dimension.y_pos):
                 self.nodes.append(Node(Vector2(i, j), True))
-
-    def find_neighbors(self):
-        current
