57d8e3cf4f72141a5cffd1830d83277699eb3917
Taurean Roberts <taureanjroberts@gmail.com>
2018-02-21 12:57:20 -0600

    Left before class

--- new_a_star.py
+++ new_a_star.py
@@ -1,16 +1,17 @@
 from vector2_class import Vector2
 class Node(object):
     def __init__(self, pos):
-        self.guid = pos
+        self.position = pos
 
 class Graph(object):
-    def __init__(self, dims):
+    def __init__(self, nodes):
         self.nodes = []
-        self.dimension = dims
+        self.dimension = nodes
 
-    def make_nodes(self):
+    def make_nodes(self, _nodes):
         '''Make a search space for the nodes'''
-        for i in range(self.dimension, self.nodes, +1):
-            self.nodes.append(Node(i))
+        for i in range(0, self.dimension, +1):
+            self.nodes.append(i)
 
-new_node = Graph(4)
+g = Graph([4])
+new_g = g.make_nodes
