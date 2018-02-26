73409ff148e0fe130622e7e373a536281423b344
Taurean Roberts <taureanjroberts@gmail.com>
2018-02-26 13:44:12 -0600

    Worked on Finding neighbors
    Took short break after commit

--- new_a_star.py
+++ new_a_star.py
@@ -12,9 +12,21 @@ class Graph(object):
         '''Make a search space for the nodes'''
         for i in range(0, self.dimension.x_pos):
             for j in range(0, self.dimension.y_pos):
-                _node = Node(Vector2(i, j))
-                self.nodes.append(_node)
+                self.nodes.append(Vector2(i, j))
+
+    def find_neighbors(self, node):
+        '''Finds nodes that neighbor the current node'''
+        top = node.guid - Graph.make_nodes #Top Node
+        bot = node.guid + Graph.make_nodes #Bottom Node
+        left = node.guid - 1 #Left Node
+        right = node.guid + 1 #Right Node
+        topl = top - 1 #TopLeft Node
+        topr = top + 1 #TopRight Node
+        botl = bot - 1 #BottomLeft Node
+        botr = bot + 1 #BottomRight Node
+        total_positions = [top, bot, left, right, topl, topr, botl, botr]
+        self.nodes.append(total_positions)
 
 g = Graph(Vector2(4, 4))
 new_g = g.make_nodes()
-a = 0
+print new_g
