43f76cb6962eafbfa623daccd7ea6b0ed3736aa6
Taurean Roberts <taureanjroberts@gmail.com>
2018-02-26 17:20:21 -0600

    guid completed
    Also worked on finding neighbors

--- new_a_star.py
+++ new_a_star.py
@@ -1,6 +1,7 @@
 from vector2_class import Vector2
 class Node(object):
-    def __init__(self, pos):
+    def __init__(self, pos, _guid):
+        self.guid = _guid
         self.position = pos
 
 class Graph(object):
@@ -12,14 +13,15 @@ class Graph(object):
         '''Make a search space for the nodes'''
         for i in range(0, self.dimension.x_pos):
             for j in range(0, self.dimension.y_pos):
-                self.nodes.append(Vector2(i, j))
+                _node = Node(Vector2(i, j), +1)
+                self.nodes.append(_node)
 
-    def find_neighbors(self, node):
+    def find_neighbors(self):
         '''Finds nodes that neighbor the current node'''
-        top = node.guid - Graph.make_nodes #Top Node
-        bot = node.guid + Graph.make_nodes #Bottom Node
-        left = node.guid - 1 #Left Node
-        right = node.guid + 1 #Right Node
+        top = self.nodes - self.dimension.x_pos #Top Node
+        bot = self.nodes + self.dimension.x_pos #Bottom Node
+        left = self.nodes - 1 #Left Node
+        right = self.nodes + 1 #Right Node
         topl = top - 1 #TopLeft Node
         topr = top + 1 #TopRight Node
         botl = bot - 1 #BottomLeft Node
