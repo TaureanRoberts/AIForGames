86bda53bfe951a0b960007aab1d57c9dd52c829a
Taurean Roberts <taureanjroberts@gmail.com>
2018-02-26 10:40:44 -0600

    Made Nodes.  No GUID
    Before working on Guid

--- new_a_star.py
+++ new_a_star.py
@@ -4,15 +4,17 @@ class Node(object):
         self.position = pos
 
 class Graph(object):
-    def __init__(self, nodes):
+    def __init__(self, dims):
         self.nodes = []
-        self.dimension = nodes
+        self.dimension = dims
 
     def make_nodes(self):
         '''Make a search space for the nodes'''
-        for i in range(0, self.dimension):
-            _nodes = Node(i)
-            self.nodes.append(_nodes)
+        for i in range(0, self.dimension.x_pos):
+            for j in range(0, self.dimension.y_pos):
+                _node = Node(Vector2(i, j))
+                self.nodes.append(_node)
 
-g = Graph([4])
-new_g = g.make_nodes
+g = Graph(Vector2(4, 4))
+new_g = g.make_nodes()
+a = 0
