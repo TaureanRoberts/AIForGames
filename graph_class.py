ac12229ba228f7cff5b739f080bb8cf554857c0a
Taurean Roberts <taureanjroberts@gmail.com>
2018-02-28 11:54:59 -0600

    Neighbors prints the nodes

--- graph_class.py
+++ graph_class.py
@@ -4,19 +4,18 @@ class Graph(object):
     def __init__(self, dims):
         self.nodes = []
         self.dimension = dims
-        self.g_score = 0
 
     def make_nodes(self):
         '''Make a search space for the nodes'''
-        guid = 0
-        for i in range(0, self.dimension.y_pos):
-            for j in range(0, self.dimension.x_pos):
-                _node = Node(Vector2(i, j), guid)
-                guid = guid + 1
+        id = 0
+        for i in range(0, self.dimension.x_pos):
+            for j in range(0, self.dimension.y_pos):
+                _node = Node(Vector2(i, j), id)
+                id = id + 1
                 self.nodes.append(_node)
 
     def find_neighbors(self, guid):
-        '''Finds nodes that neighbor the current node'''
+        '''Finds nodes that neighbor the current node'''    
         top = guid - self.dimension.x_pos  # Top Node
         bot = guid + self.dimension.x_pos  # Bottom Node
         left = guid - 1  # Left Node
@@ -25,9 +24,14 @@ class Graph(object):
         topr = top + 1  # Top Right node
         botl = bot - 1  # Bottom Left node
         botr = bot + 1  # Bottom Right node
-        total_positions = [top, bot, left, right, topl, topr, botl, botr]
-        self.nodes.append(total_positions)
+        total_pos = [top, bot, left, right, topl, topr, botl, botr]
+        neighbors = []
+        for node in self.nodes:
+            if total_pos.__contains__(node.guid):
+                neighbors.append(node)
+        return neighbors
 
-g = Graph(Vector2(4, 4))
-new_g = g.find_neighbors(5)
+graph_size = Graph(Vector2(4, 4))
+graph_size.make_nodes()
+new_g = graph_size.find_neighbors(5)
 print new_g
